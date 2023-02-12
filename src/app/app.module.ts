012418c34cef826857245ed12d05e0b737a98e51
Jeyson <jeysoon@gmail.com>
2023-02-12 03:43:19 -0700

    Login page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './modules/login.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        LoginModule,
+        NgbModule,
+        ReactiveFormsModule
+    ]
 })
 export class AppModule { }
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  formularioLogin: FormGroup
+  constructor() {
+    const controles: any = {
+      correo: new FormControl('', [Validators.required, Validators.email]),
+      contrasena: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    }
+    this.formularioLogin = new FormGroup(controles)
+
+   }
+
+  ngOnInit() {
+  }
+  login(){
+    console.log(this.formularioLogin);
+  }
+  getFormStatus(){
+    return this.formularioLogin.valid ? 'btn btn-primary' : 'btn btn-secondary'
+  }
+}
